f4c5d5a84f6eebd0d456d4ae52c64aa5928e1c38
Manfred Touron <m@42.am>
2014-07-09 17:05:16 +0200

    updated jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+set -e
+
 npm --color false install
-coffeelint --jslint server.coffee | sed -e 's/\?>/\?>\n/g' -e 's/lineEnd=undefined//g'| tee jslint.xml
+coffeelint --report jslint server.coffee | sed -e 's/\?>/\?>\n/g' -e 's/lineEnd=undefined//g'| tee jslint.xml
+
+rm -rf dist artifacts
+mkdir -p dist/static artifacts
+
+coffee -o dist/ -c server.coffee
+
+for file in static/index.html static/tty.js package.json; do
+    cp "$file" "dist/$file"
+done
+
+PWD=$(pwd)
+
+cd dist
+npm install
+cd ..
+
+tar czf artifacts/dist.tgz dist
